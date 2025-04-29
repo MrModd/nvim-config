d0ca525c59f71ca27a6b721628bc701e0c0c19df
Federico Cosentino <mr.modd@gmail.com>
2025-04-29 11:25:47 +0200

    New keymap: F7 performs a git grep on the word under the cursor

--- lua/plugins/gitgutter.lua
+++ lua/plugins/gitgutter.lua
@@ -5,5 +5,11 @@
 -- https://github.com/airblade/vim-gitgutter
 
 return {
-  "airblade/vim-gitgutter"
+  "airblade/vim-gitgutter",
+  config = function()
+    vim.keymap.set('n', '<F7>', function()
+      local word = vim.fn.expand('<cword>')
+      vim.cmd('Git grep ' .. vim.fn.shellescape(word))
+    end, { desc = "Git grep the word under cursor" })
+  end
 }
