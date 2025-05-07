1ae1eb45d9ae883bc9f37ab93856d09ef30791b7
Federico Cosentino <mr.modd@gmail.com>
2025-05-07 17:52:23 +0200

    Added full path and lsp status in status bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,6 +11,10 @@ return {
     require("lualine").setup({
       options = {
         theme = "gruvbox"
+      },
+      sections = {
+        lualine_c = {{'filename', path=1}},
+        lualine_x = {'encoding', 'fileformat', 'filetype', 'lsp_status'},
       }
     })
   end
