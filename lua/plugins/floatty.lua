8fef571d18f50b55fe8263de2f62623022158fda
Federico Cosentino <mr.modd@gmail.com>
2026-02-25 10:48:21 +0100

    Minor changes on Floatty config

--- lua/plugins/floatty.lua
+++ lua/plugins/floatty.lua
@@ -8,15 +8,14 @@ return {
   "ingur/floatty.nvim",
   lazy = false, -- load at startup so codex can be spawned anytime
   config = function()
-    local floatty = require("floatty").setup({
+    local terminal = require("floatty").setup({
       width = 0.9,
       height = 0.9,
       border = "rounded",
       cmd = vim.o.shell,  -- Replace with the command of choice
     })
 
-    -- Toggle Codex terminal (starts codex if not already running)
-    vim.keymap.set("n", "<F8>", function() floatty.toggle() end, { desc = "Persistent float terminal" })
-    vim.keymap.set("t", "<F8>", function() floatty.toggle() end, { desc = "Persistent float terminal" })
+    vim.keymap.set("n", "<F8>", function() terminal.toggle() end, { desc = "Floatty: persistent float terminal" })
+    vim.keymap.set("t", "<F8>", function() terminal.toggle() end, { desc = "Floatty: persistent float terminal" })
   end,
 }
