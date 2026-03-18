a9db40defb4250f8c50cfed4db8b6f4c6d681134
Federico Cosentino <mr.modd@gmail.com>
2026-03-18 09:56:20 +0100

    Added new shortcut on Telescope
    The shortcut opens a Telescope floating window with the files
    in the folder of the current open file.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,6 +17,40 @@ return {
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope: buffers" })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope: help tags" })
       vim.keymap.set("n", "<C-x>", "<Cmd>Telescope keymaps<CR>", { desc = "Telescope: show all keybinds" })
+
+      -- BEGIN: open Telescope in the current file's directory and open the selected file in an horizontal split
+      local actions = require("telescope.actions")
+      local actions_state = require("telescope.actions.state")
+      local function open_fuzzy_in_cwd_vsplit()
+        local cwd = vim.fn.expand("%:p:h")
+        if cwd == "" or cwd == "." then cwd = vim.loop.cwd() end
+
+        local opts = {
+          cwd = cwd,
+          hidden = true,
+          prompt_title = "Files in: " .. cwd,
+          attach_mappings = function(prompt_bufnr, map)
+            local open_in_vsplit = function()
+              local selection = actions_state.get_selected_entry()
+              if not selection then return end
+              local target = selection.path or selection.filename or selection.value
+              if not target then target = tostring(selection) end
+              actions.close(prompt_bufnr)
+              vim.cmd("split " .. vim.fn.fnameescape(target))
+            end
+            map("i", "<CR>", open_in_vsplit)
+            map("n", "<CR>", open_in_vsplit)
+            return true
+          end,
+        }
+
+        builtin.find_files(opts)
+      end
+
+      vim.api.nvim_create_user_command("TelescopeCwdSplit", open_fuzzy_in_cwd_vsplit, {})
+      vim.keymap.set("n", "<leader>t", open_fuzzy_in_cwd_vsplit, { desc = "Telescope: find files in current file's directory and open in split" })
+      -- END
+
     end
   },
   {
